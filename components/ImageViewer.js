a8d1cee6953abee0f45d382c4e8b8b4107ec227f
zhangdi <1258956799@qq.com>
2023-11-25 00:24:46 +0800

    initial commit

---
+++ components/ImageViewer.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Image,} from 'react-native';
+
+export default function ImageViewer({ PlaceholderImage }) {
+
+  return (
+    <Image source={PlaceholderImage} style={styles.image} />
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
